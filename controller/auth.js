1fff70d62c244953fe10e44aa94aa410a584d733
hanifrff <hanifrff@gmail.com>
2023-06-10 19:24:40 +0700

    CHeckpoint 2

--- controller/auth.js
+++ controller/auth.js
@@ -1,13 +1,45 @@
-const userModel = require("../models/user")
-
-
 module.exports = {
   register(req, res) {
     const { username, email, phone, password, confirmPassword } = req.body;
-    
+    const { user } = require("../models");
 
-    res.send({
-      message: "succsess",
-    });
+    // Lakukan check jika password dan confirmPassword, tidak sama
+    if (password !== confirmPassword) {
+      res.status(400).send({
+        message: "error: password and confirm password is not equal",
+      });
+      return;
+    }
+
+    // Lakukan check: tidak ada email yang boleh sama
+    // Lakukan select * from users where email = 'denoxet582@larland.com';
+    // Lakukan user.findAll
+    user
+      .findAll({
+        where: {
+          email: email,
+        },
+      })
+      .then((data) => {
+        res.status(400).send({
+          message: "error: email has already taken",
+        });
+
+        
+      });
+
+    // Lakukan insert
+    user
+      .create({ username, email })
+      .then((data) => {
+        res.send({
+          message: "succsess",
+        });
+      })
+      .catch((e) => {
+        res.send({
+          message: "fail",
+        });
+      });
   },
 };
